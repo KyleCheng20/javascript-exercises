728a152b2993542e3b422dcf408424d6f1eb2795
Kyle Cheng <kcheng120@gmail.com>
2025-10-01 20:25:23 -0400

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = arr.filter(item => {
+        for(let i = 0; i < args.length; i++){
+            if(item === args[i]){
+                return false;
+            }
+        }
+        return true;
+    })
+    return newArr;
 };
 
 // Do not edit below this line

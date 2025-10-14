c6c18e48c09475b9860e96ee327d16260c392c7a
Kyle Cheng <kcheng120@gmail.com>
2025-10-14 17:27:48 -0400

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let strArray1 = newStr.split("");
 
+    let j = strArray1.length - 1;
+    for(let i = 0; i < strArray1.length; i++){
+        if(strArray1[i] === strArray1[j]){
+            j--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;

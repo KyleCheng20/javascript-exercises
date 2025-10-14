5619381fc75b3664046d9a209b49028e40d3515f
Kyle Cheng <kcheng120@gmail.com>
2025-10-14 16:30:45 -0400

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length === 1){
+    return arr[0];
+  }
+	return arr.reduce((acc, num) => {
+    return acc + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, num) => {
+    return acc * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  let total = 1;
+  if(num === 0){
+    return total;
+  }
+  else if(num === 1){
+    return total;
+  }
+
+  let i = num;
+  while(i >= 1){
+    total *= i;
+    i--;
+  }
+  return total;
+}
 
 // Do not edit below this line
 module.exports = {

e56065c9973fcadd2420eac0bfe1a3737726632b
Kyle Cheng <kcheng120@gmail.com>
2025-10-15 16:39:08 -0400

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    let current = 1;
+    let prev = 1;
 
+    if(num == 0){
+        return current = 0;
+    }
+    else if(num < 0){
+        return "OOPS";
+    }
+
+    for(let i = 2; i < num; i++){
+        let next = current + prev;
+        prev = current;
+        current = next;
+    }
+    return current;
+    
 };
 
 // Do not edit below this line

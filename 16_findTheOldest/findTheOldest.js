1a6740df4b151327b6f9f65824a806bcba89d73b
Kyle Cheng <kcheng120@gmail.com>
2025-10-16 13:32:17 -0400

    Complete 15_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, curr) => {
+        if(acc.yearOfDeath === undefined){
+            acc.yearOfDeath = new Date().getFullYear();
+        }
+        let accAge = acc.yearOfDeath - acc.yearOfBirth;
 
+        if(curr.yearOfDeath === undefined){
+            curr.yearOfDeath = new Date().getFullYear();
+        }
+        let currAge = curr.yearOfDeath - curr.yearOfBirth;
+
+        if(accAge > currAge){
+            return acc;
+        }
+        else{
+            return curr;
+        }
+
+
+    }, arr[0]);
 };
 
 // Do not edit below this line

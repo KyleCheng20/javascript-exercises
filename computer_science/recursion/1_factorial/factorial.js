c058bcc4c2edb3c57896b27784605e4defd2073e
Kyle Cheng <kcheng120@gmail.com>
2026-02-09 17:09:03 -0500

    Complete 1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if(n === 1 || n === 0){
+        return 1;
+    } 
+    if(Number.isInteger(n) && n > 0){
+        return n * factorial(n - 1);
+    } else{
+        return undefined;
+    }
 };
 
 // Do not edit below this line

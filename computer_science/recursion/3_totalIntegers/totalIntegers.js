743d71f71a4cf4fe5726264710a2dd5fba0bfb74
Kyle Cheng <kcheng120@gmail.com>
2026-02-10 16:32:43 -0500

    Complete 3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,13 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(data) {
+    if(typeof data !== "object" || data === null) return undefined;
+
+    let count = 0;
+
+    for(let value of Object.values(data)){
+        if(Number.isInteger(value)) count++;
+        else if(typeof value === "object" && value !== null) count += totalIntegers(value);
+    }
+    return count;
 };
   
 // Do not edit below this line

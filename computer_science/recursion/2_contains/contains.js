aa3b436dd15e9e26397e7a3e93f5fd1957f55634
Kyle Cheng <kcheng120@gmail.com>
2026-02-10 15:42:20 -0500

    Complete 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,13 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+    if(obj === value || (Number.isNaN(obj) && Number.isNaN(value))) return true;
+    if(typeof obj !== "object" || obj === null) return false;
+    
+    for(let subObj of Object.values(obj)){
+        if(contains(subObj, value)){
+            return true;
+        }
+    }
+    return false;
 };
   
 // Do not edit below this line

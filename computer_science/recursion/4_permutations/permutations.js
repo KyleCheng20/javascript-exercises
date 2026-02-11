1aefb10a701d2d10f41b14576be73859e1411d28
Kyle Cheng <kcheng120@gmail.com>
2026-02-10 23:35:48 -0500

    Complete 4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,17 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+    if(arr.length === 0) return [[]];
+
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        let currElement = arr[i];
+        let remaining = arr.slice(0, i).concat(arr.slice(i+1));
+        let permOfRemaining = permutations(remaining);
+        
+        for(let perm of permOfRemaining){
+            result.push([currElement, ...perm]);
+        }
+    }
+    return result;
 };
   
 // Do not edit below this line

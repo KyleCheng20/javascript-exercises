b922e98430b4d418c79afaf03c5724f9829ddca1
Kyle Cheng <kcheng120@gmail.com>
2026-02-11 13:58:29 -0500

    Ccomplete 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,18 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    if(n === 1) return [1];
+
+    let prevRow = pascal(n - 1);
+    let newRow = [];
+
+    newRow.push(1);
+
+    for(let i = 0; i < prevRow.length - 1; i++){
+        newRow.push(prevRow[i] + prevRow[i+1]);
+    }
+
+    newRow.push(1);
+
+    return newRow;
 };
   
 // Do not edit below this line
